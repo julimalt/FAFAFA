20baacba7c82d274ff6fda02843be2b2c24cdd43
julimalt <59095183+julimalt@users.noreply.github.com>
2020-07-19 18:06:41 -0300

    dia 3

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
 var showDiv = () => {
   console.log("\n\nurl=>", window.location.href);
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get("type");
+  console.log("\n\ntype=>", myParam);
 };
