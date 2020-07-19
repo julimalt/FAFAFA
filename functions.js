4f5cad6f5842e86e540f9784582fb6417c3c18e9
julimalt <59095183+julimalt@users.noreply.github.com>
2020-07-19 20:04:47 -0300

    dia 3

--- functions.js
+++ functions.js
@@ -11,4 +11,26 @@ const showDiv = () => {
 const redirectPage = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const myParam = urlParams.get("type");
+  const select = document.querySelector("select");
+  let respuesta = 0;
+
+  for (let i = 0; i < select.options.length; i++) {
+    let option = select.options[i];
+    if (option.selected) respuesta = option.value;
+  }
+  console.log(respuesta);
+
+  let url = "";
+
+  switch(respuesta){
+      case 1:
+      case 2:
+        url = "situacion2.html?" + "type=" + myParam + "&caso=" + respuesta;
+        break;
+        case 3:
+            case 4: 
+            case 
+  }
+
+  window.location.href = 
 };
