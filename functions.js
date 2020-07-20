13a1ffd321bed51a32998c055d7345c4ea8fdf2b
julimalt <59095183+julimalt@users.noreply.github.com>
2020-07-19 23:04:17 -0300

    dia 3

--- functions.js
+++ functions.js
@@ -8,7 +8,7 @@ const showDiv = () => {
   buttonResponder.style.display = "inherit";
 };
 
-const redirectPage = (url1, url2) => {
+const redirectPage = (url1, url2, url3, url4) => {
   const urlParams = new URLSearchParams(window.location.search);
   const myParam = urlParams.get("type");
   const allInputs = document.querySelectorAll("input[type=radio]");
@@ -25,11 +25,17 @@ const redirectPage = (url1, url2) => {
 
   switch (respuesta) {
     case "1":
-    case "2":
       url = url1 + "?" + newParams;
       break;
+    case "2":
+      url = url2 + "?" + newParams;
+      break;
     case "3":
+      url = url3 + "?" + newParams;
+      break;
     case "4":
+      url = url4 + "?" + newParams;
+      break;
     case "0":
       url = url2 + "?" + newParams;
       break;
