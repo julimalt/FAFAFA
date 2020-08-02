7232c8fafac93d87fe75bffae7385d1be2c1a712
julimalt <59095183+julimalt@users.noreply.github.com>
2020-08-02 14:09:03 -0300

    dia 16

--- js/functions.js
+++ js/functions.js
@@ -23,6 +23,12 @@ const showDiv = () => {
   }, 16000);
 };
 
+//función que nos cambie el horario del div que contiene el reloj dependiendo el parámetro hs
+// const agregarHoraYRedirigir = (url1, url2, url3, url4, hora) => {
+//   calcularHora(hora);
+//   redirectPage(url1, url2, url3, url4);
+// };
+
 const redirectPage = (url1, url2, url3, url4, hs) => {
   /*Obtenemos una lista con todos los tags que son del tipo <input type="radio">*/
   const allInputs = document.querySelectorAll("input[type=radio]");
@@ -70,8 +76,56 @@ const goToPage = (page, hs) => {
   window.location.href = page + "?" + newParams;
 };
 
-//función que nos cambie el horario del div que contiene el reloj dependiendo el parámetro hs
-// const agregarHoraYRedirigir = (url1, url2, url3, url4, hora) => {
-//   calcularHora(hora);
-//   redirectPage(url1, url2, url3, url4);
-// };
+const getHourUpdated = () => {
+  let horasAdicionales = 0;
+  let minutosAdicionales = 0;
+
+  /*Obtenemos los parámetros de la URL actual*/
+  const urlParams = new URLSearchParams(window.location.search);
+
+  /*Obtenemos el valor del parámetro de la URL llamado hs*/
+  let hoursParam = urlParams.get("hs");
+
+  /*Pasamos el valor de hoursParam a int*/
+  hoursParam = parseFloat(hoursParam);
+
+  /*Obtenemos el tag span con el id "horas"*/
+  const objetoHoras = document.getElementById("horas");
+
+  /*Obtenemos el valor del span id "reloj" y pasamos el texto a int*/
+  const horaActual = parseInt(objetoHoras.innerText);
+
+  /*Obtenemos el tag span con el id "horas"*/
+  const objetoMinutos = document.getElementById("minutos");
+
+  /*Obtenemos el valor del span id "reloj" y pasamos el texto a int*/
+  let minutosActual = parseInt(objetoMinutos.innerText);
+
+  /*horasAdicionales va a tener el valor entero de las horas que llegaron por parámetro de la url*/
+  horasAdicionales = Math.floor(hoursParam);
+
+  /*nuevaHora va a tener la suma de la hora actual del span + las horasAdicionales*/
+  let nuevaHora = horaActual + horasAdicionales;
+
+  /*horasAdicionales va a tener el valor flotante de las horas que llegaron por parámetro de la url*/
+  minutosAdicionales = hoursParam % 1;
+
+  /*Checkeo si los minutosAdicionales recibidos por parámetro de la url son 0.5,
+  si lo son hago que minutosActual se le sumen 30*/
+  if (minutosAdicionales === 0.5) {
+    minutosActual = minutosActual + 30;
+  }
+
+  /*Checkeo si los minutosActual es igual a 60,
+  si lo es le sumo 1 a nuevaHora y le asigno a minutosActual "00"*/
+  if (minutosActual === 60) {
+    nuevaHora = nuevaHora + 1;
+    minutosActual = "00";
+  }
+
+  /*Le asigno el valor de nuevaHora al span con id "horas"*/
+  objetoHoras.innerHTML = nuevaHora;
+
+  /*Le asigno el valor de minutosActual al span con id "minutos"*/
+  objetoMinutos.innerHTML = minutosActual;
+};
